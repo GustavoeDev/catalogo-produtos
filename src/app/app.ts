1b006bc464c35866ec02f79bd8efba1fb94e8592
Gustavo Emanuel Silva Queiroz <gustavoe.dev@gmail.com>
2026-05-06 20:11:07 -0300

    remove imports inutilizados

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ThemeToggle} from './components/theme-toggle/theme-toggle';
-import {Home} from './pages/home/home';
 import {Header} from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ThemeToggle, Home, Header],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import {ThemeToggle} from '../theme-toggle/theme-toggle';
 import { MatIcon } from '@angular/material/icon';
 import {RouterLink, RouterLinkActive} from '@angular/router';
-import {MatIconButton} from '@angular/material/button';
 import {MatBadgeModule} from '@angular/material/badge';
 
 @Component({
   selector: 'app-header',
-  imports: [ThemeToggle, RouterLink, RouterLinkActive, MatIconButton, MatIcon, MatBadgeModule],
+  imports: [ThemeToggle, RouterLink, RouterLinkActive, MatIcon, MatBadgeModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
